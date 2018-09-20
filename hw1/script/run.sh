1e6447d29e470626cb36c081fd681a40b63a9391
chenfh5 <chenfh5@qq.com>
2018-09-20 10:45:53 +0800

    ADD: add main controller

---
+++ hw1/script/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -cp target/hw1.jar io.github.chenfh5.Controller 99
---
+++ hw1/src/main/scala/io/github/chenfh5/Controller.scala
@@ -0,0 +1,75 @@
+package io.github.chenfh5
+
+import io.github.chenfh5.agg.mr.MRAggregation
+import io.github.chenfh5.agg.spark.SparkAggregation
+import io.github.chenfh5.common.{OwnUtils, SparkEnvironment}
+import io.github.chenfh5.wc.mr.MRWc
+import io.github.chenfh5.wc.spark.SparkWc
+import org.apache.commons.lang3.StringUtils
+import org.apache.spark.SparkContext
+
+object Controller {
+
+  def main(args: Array[String]): Unit = {
+    require(StringUtils.isNoneBlank(System.getenv("JAVA_HOME")))
+    require(StringUtils.isNoneBlank(System.getenv("HADOOP_HOME"))) // winutils.ext in case of Windows OS, and restart IDE
+
+    args.head.toInt match {
+      case 11 => testMRWc()
+      case 12 => testSparkWc()
+      case 21 => testMRAgg()
+      case 22 => testSparkAgg()
+      case _ => println(help())
+    }
+  }
+
+  def help(): String = {
+    """
+      |USAGE:
+      |11 => MRWc()
+      |12 => SparkWc()
+      |21 => MRAgg()
+      |22 => SparkAgg()
+    """.trim.stripMargin
+  }
+
+  def initSparkContext(): SparkContext = {
+    System.setProperty("spark.app.name", "Spark Unit Test")
+    System.setProperty("spark.master", "local[2]")
+    val conf = SparkEnvironment.getSparkConf
+    new SparkContext(conf)
+  }
+
+  def testMRWc(): Unit = {
+    val srcPath = OwnUtils.makeFile(OwnUtils.getCurrentDir, "input", "GPL3_LICENSE")
+    val destPath = OwnUtils.makeFile(OwnUtils.getCurrentDir, "output", "mr", "wc")
+    val res = MRWc.process(srcPath, destPath)
+    println(res)
+  }
+
+  def testSparkWc(): Unit = {
+    val sc = initSparkContext()
+    val srcPath = OwnUtils.makeFile(OwnUtils.getCurrentDir, "input", "GPL3_LICENSE")
+    val destPath = OwnUtils.makeFile(OwnUtils.getCurrentDir, "output", "spark", "wc")
+    val wc = SparkWc()
+    val res: Unit = wc.process(srcPath, destPath, sc)
+    println(res)
+  }
+
+  def testMRAgg(): Unit = {
+    val srcPath = OwnUtils.makeFile(OwnUtils.getCurrentDir, "input", "edges.csv")
+    val destPath = OwnUtils.makeFile(OwnUtils.getCurrentDir, "output", "mr", "agg")
+    val res = MRAggregation.process(srcPath, destPath)
+    println(res)
+  }
+
+  def testSparkAgg(): Unit = {
+    val sc = initSparkContext()
+    val srcPath = OwnUtils.makeFile(OwnUtils.getCurrentDir, "input", "edges.csv")
+    val destPath = OwnUtils.makeFile(OwnUtils.getCurrentDir, "output", "spark", "agg")
+    val agg = SparkAggregation()
+    val res: Unit = agg.process(srcPath, destPath, sc)
+    println(res)
+  }
+
+}
