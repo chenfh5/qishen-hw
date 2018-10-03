a377ea794143c1fcd354b324ca24214e7587be77
chenfh5 <chenfh5@qq.com>
2018-10-03 23:58:36 +0800

    ADD: add maxFilter && triangle join

--- hw1/src/main/scala/io/github/chenfh5/agg/spark/SparkAggregationHW2.scala
+++ hw1/src/main/scala/io/github/chenfh5/agg/spark/SparkAggregationHW2.scala
@@ -6,14 +6,20 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SaveMode
 import org.slf4j.LoggerFactory
 
-class SparkAggregationHW2() {
+
+class SparkAggregationHW2(maxFilterValue: Long = Int.MaxValue) {
   private val LOG = LoggerFactory.getLogger(getClass)
   private val sc: SparkContext = SparkEnvironment.getSparkSession.sparkContext
 
-  def getInput(srcPath: String): RDD[Array[String]] = {
+  def save(userFollower: RDD[(String, Int)], destPath: String): Unit = {
+    userFollower.sortBy(e => e._1).coalesce(1).saveAsTextFile(destPath)
+  }
+
+  def getInput(srcPath: String) = {
     sc.textFile(srcPath)
       .map(line => line.split(","))
       .filter(_.head matches """\d+""") // redundant
+      .filter(e => SparkAggregationHW2.maxFilter(e, maxFilterValue))
   }
 
   def RDDG(srcPath: String, destPath: String): Unit = {
@@ -71,12 +77,51 @@ class SparkAggregationHW2() {
     userFollower.sort("_1").select(concat_ws(",", col("_1"), col("cnt"))).coalesce(1).write.mode(SaveMode.Overwrite).format("text").save(destPath)
   }
 
-  def save(userFollower: RDD[(String, Int)], destPath: String): Unit = {
-    userFollower.sortBy(e => e._1).coalesce(1).saveAsTextFile(destPath)
+
+  def DSTRIANGLE(srcPath: String, destPath: String): (Long, Double) = {
+    LOG.info("this is the DSTRIANGLE srcPath={}, destPath={}", Seq(srcPath, destPath): _*)
+
+    val sparkSession = SparkEnvironment.getSparkSession
+    import org.apache.spark.sql.functions._
+    import sparkSession.implicits._
+
+    val userFollower = getInput(srcPath)
+      .map(e => (e.last, e.head))
+      .toDS()
+
+    val xy = userFollower.select($"_1".as("x"), $"_2".as("y"))
+    val yz = userFollower.select($"_1".as("y"), $"_2".as("z"))
+    val zx = userFollower.select($"_1".as("z"), $"_2".as("x"))
+    val xyz = xy.as("xy").join(yz.as("yz"), col("xy.y") === col("yz.y")).drop(col("yz.y"))
+    val triangle = xyz.as("xyz").join(zx.as("zx"), Seq("z", "x")).drop(col("zx.z")).drop(col("zx.x"))
+    // Make sure that your program does not triple-count the same triangle.
+    val distTriangle = triangle
+      .map { row =>
+        val (x, y, z) = (row.getAs[String]("x").toLong, row.getAs[String]("y").toLong, row.getAs[String]("z").toLong)
+        val minVal = math.min(math.min(x, y), z)
+        val maxVal = math.max(math.max(x, y), z)
+        val midVal = x + y + z - minVal - maxVal
+        (minVal, midVal, maxVal)
+      }
+      .distinct()
+      .cache()
+
+    distTriangle.select(concat_ws(",", $"_1", $"_2", $"_3")).coalesce(1).write.mode(SaveMode.Overwrite).format("text").save(destPath)
+
+    val cnt = distTriangle.count()
+    val cntDiv3 = cnt / 3d
+    LOG.info("this is the DSTRIANGLE cnt={}, cntDiv3={}", cnt, cntDiv3)
+    (cnt, cntDiv3)
   }
 
 }
 
 object SparkAggregationHW2 {
-  def apply(): SparkAggregationHW2 = new SparkAggregationHW2()
+
+  def maxFilter(oneRow: Array[String], maxValue: Long): Boolean = {
+    if (oneRow.head.toLong <= maxValue && oneRow.last.toLong <= maxValue) true else false
+  }
+
+  def apply(maxFilterValue: Long = Int.MaxValue): SparkAggregationHW2 = new SparkAggregationHW2(maxFilterValue)
+
 }
